818a662beecb9bd41f1f3e8d88ea8661aca52bb7
unknown <rohit1110>
2018-06-21 19:23:21 +0530

    login title

--- Bill/app/src/main/java/com/reso/bill/LoginActivity.java
+++ Bill/app/src/main/java/com/reso/bill/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -69,7 +70,9 @@ public class LoginActivity extends AppCompatActivity implements
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainlogin);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.ltoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
         mPhoneNumberField = (EditText) findViewById(R.id.field_phone_number);
         mVerificationField = (EditText) findViewById(R.id.field_verification_code);
         mCodeNumberField=(TextView) findViewById(R.id.field_code_number);
@@ -85,6 +88,8 @@ public class LoginActivity extends AppCompatActivity implements
         mStartButton.setOnClickListener(this);
         mVerifyButton.setOnClickListener(this);
         mResendButton.setOnClickListener(this);
+
+
 
         mAuth = FirebaseAuth.getInstance();
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
