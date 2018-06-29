5b6a01a2ef0377418013edbd34f103d7626c4ccc
unknown <rohit1110>
2018-06-29 19:21:15 +0530

    expand textview

--- Bill/app/src/main/java/com/reso/bill/SelectNewspaper.java
+++ Bill/app/src/main/java/com/reso/bill/SelectNewspaper.java
@@ -1,10 +1,12 @@
 package com.reso.bill;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -58,6 +60,43 @@ public class SelectNewspaper extends Fragment {
         });
         txtSelectedItems = (TextView) rootView.findViewById(R.id.txt_selected_items);
         txtSelectedItems.setText("");
+        txtSelectedItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater li = LayoutInflater.from(getActivity());
+                View promptsView = li.inflate(R.layout.dialog_layout, null);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        getActivity());
+                alertDialogBuilder.setView(promptsView);
+                final TextView userInput = (TextView) promptsView
+                        .findViewById(R.id.textView1);
+                userInput.setText(txtSelectedItems.getText().toString());
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+
+                                    }
+                                });
+                        /*.setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });*/
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
         return rootView;
     }
 
