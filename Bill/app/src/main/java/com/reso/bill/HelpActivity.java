90bea7a1495ddd8553809a331224fd6d98e226ba
Hemant <hemant1kstar@gmail.com>
2019-05-22 11:57:13 +0530

    Taking toolbar title now from intent extras

--- Bill/app/src/main/java/com/reso/bill/HelpActivity.java
+++ Bill/app/src/main/java/com/reso/bill/HelpActivity.java
@@ -16,18 +16,20 @@ public class HelpActivity extends AppCompatActivity {
     private WebView webView;
 
     String intentUrl;
+    String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        if (getSupportActionBar() != null) {
-            Utility.setActionBar("Help", getSupportActionBar());
-        }
-
         Intent intent = getIntent();
         intentUrl = intent.getStringExtra("URL");
+        title = intent.getStringExtra("ACTIVITY_TITLE");
+
+        if (getSupportActionBar() != null) {
+            Utility.setActionBar(title, getSupportActionBar());
+        }
 
         webView = findViewById(R.id.web_help_page);
         webView.setWebViewClient(new MyBrowser());
