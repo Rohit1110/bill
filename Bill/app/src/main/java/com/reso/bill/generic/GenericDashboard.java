8da1fdeeb7bbe61ad8c81fe56078ed2021110c57
Hemant <hemant1kstar@gmail.com>
2019-05-22 12:13:29 +0530

    Removed help link from side drawer

--- Bill/app/src/main/java/com/reso/bill/generic/GenericDashboard.java
+++ Bill/app/src/main/java/com/reso/bill/generic/GenericDashboard.java
@@ -297,12 +297,6 @@ public class GenericDashboard extends AppCompatActivity implements NavigationVie
 //                fragment = Utility.getHomeFragment(user);
                 fragment = HomeFragment.newInstance(user);
                 break;
-            case R.id.nav_help:
-                Intent helpI = new Intent(GenericDashboard.this, HelpActivity.class);
-                helpI.putExtra("URL", "https://payperbill.in/help/");
-                helpI.putExtra("ACTIVITY_TITLE", "Help");
-                startActivity(helpI);
-                break;
             case R.id.nav_pending_invoices:
                 fragment = FragmentInvoiceSummary.newInstance(user);
                 break;
